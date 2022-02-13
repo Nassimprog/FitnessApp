86d4e1797e89e5e2b93ee6f814ec9d23b4a211bf
Nassimprog <56696078+Nassimprog@users.noreply.github.com>
2022-02-13 22:44:54 +0000

    Created folders for: ViewModels, Views, Navigation and implemented NavigationDispatcher.cs

---
+++ FitnessApp/FitnessApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using FitnessApp.Navigation;
+using FitnessApp.Views;
+
+namespace FitnessApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}

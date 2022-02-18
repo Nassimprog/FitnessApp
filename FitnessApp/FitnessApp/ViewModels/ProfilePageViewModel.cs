6055addb37945c6260a39750cd683dce3bf06cb4
Nassimprog <56696078+Nassimprog@users.noreply.github.com>
2022-02-18 22:40:16 +0000

    added navigation for profile page to tracker page

--- FitnessApp/FitnessApp/ViewModels/ProfilePageViewModel.cs
+++ FitnessApp/FitnessApp/ViewModels/ProfilePageViewModel.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using FitnessApp.Navigation;
+using FitnessApp.Views;
 
 using Xamarin.Forms;
 
@@ -11,7 +13,14 @@ namespace FitnessApp.ViewModels
     {
         public ProfilePageViewModel()
         {
-            
+            NavigateToTrackerCommand = new Command(NavigateToTrackerPage);
+        }
+        public Command NavigateToTrackerCommand { get; set; }
+
+        public void NavigateToTrackerPage() //not unit testable
+        {
+            var trackerpage = new TrackerPage();
+            NavigationDispatcher.Instance.Navigation.PushAsync(trackerpage);
         }
     }
 }

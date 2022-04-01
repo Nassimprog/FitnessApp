4b5f3648bdd7f54c4aabbf2a72d88de7800fff2a
Nassimprog <56696078+Nassimprog@users.noreply.github.com>
2022-04-01 15:36:38 +0100

    starting with footstep counter and accelerometer integration

--- FitnessApp/FitnessApp/ViewModels/TrackerPageViewModel.cs
+++ FitnessApp/FitnessApp/ViewModels/TrackerPageViewModel.cs
@@ -16,7 +16,10 @@ namespace FitnessApp.ViewModels
     public class TrackerPageViewModel : BaseViewModel
     {
         IAuth auth;
+        public bool EnabledTracking = false;
+        public int Footsteps = 0;
         public Command NavigateToProgressCommand { get; set; }
+        public Command BeginTrackerCommand { get; set; }
 
         public Command SignOutCommand { get; set; }
 
@@ -26,9 +29,26 @@ namespace FitnessApp.ViewModels
             SignOutCommand = new Command(SignOutFireBase);
         }
 
+        public void BeginTracker()
+        {
+             // If tracking permission has been given, do
+            
+            Accelerometer.ReadingChanged += Accelerometer_readingChanged;
+            Accelerometer.Start(SensorSpeed.UI);
+            
+                
+            Accelerometer.ReadingChanged -= Accelerometer_readingChanged;
+            Accelerometer.Stop();
+            
+        }
+        ///On accelerometer measurements update
+        private void Accelerometer_readingChanged(object sender, AccelerometerChangedEventArgs e)
+        {
+            // if the magnitude of acceleration in the X or Y axis is 1
+            if (e.Reading.Acceleration.X >= 1 || e.Reading.Acceleration.Y >= 1 || e.Reading.Acceleration.X >= -1 || e.Reading.Acceleration.Y >= -1)
+            {
 
-        
-
-        
+            }
+        }
     }
 }
